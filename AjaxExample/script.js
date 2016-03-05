07a83f543954c623e5c01c6892cb17d414dd60d9
Kumar Nitesh <knitesh@users.noreply.github.com>
2016-03-05 13:07:17 -0500

    Added example of using Promises

--- AjaxExample/script.js
+++ AjaxExample/script.js
@@ -3,16 +3,32 @@ $(function(){
 
 var url = "https://api.github.com/users/knitesh";
 
-$.get(url,function(data){
-	//console.log(data);
-	//JSON.stringify(data)
-	//json.Parse();
+// $.get(url,function(data){
+// 	//console.log(data);
+// 	//JSON.stringify(data)
+// 	//json.Parse();
+// 	for(var info in data){
+// 		$("#userInformation").append("<li class='blueclass'>"+info +" : " +data[info]+"</li>")
+// 		// window.document.writeln(info +":" +data[info]+"<br/>");
+// 	}
+// });
+
+//Using Promises
+$.get(url)
+.done(function(data){
 	for(var info in data){
 		$("#userInformation").append("<li class='blueclass'>"+info +" : " +data[info]+"</li>")
 		// window.document.writeln(info +":" +data[info]+"<br/>");
 	}
+})
+.fail(function(err){
+	alert("An error occured");
+})
+.always(function(){
+	console.info("finished");
 });
 
+//Code to change color at run time
 	$('#chnagecolor').click(function(){
 	console.log("Clicked");
 
